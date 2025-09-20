ff5d349c5981ad40bf8e1ca17955c9412857263b
IT24102454 <it24102454@my.sliit.lk>
2025-09-21 00:25:38 +0530

    project structure

---
+++ src/main/java/com/example/boatsafariweb/BoatSafariWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.boatsafariweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatSafariWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoatSafariWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boatsafariweb/BoatSafariWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boatsafariweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoatSafariWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
